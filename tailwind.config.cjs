8078ade4d08ab2d3ef72e0c2744c2bba6155cde8
Pavezi <pavezivinicius@gmail.com>
2024-03-29 09:54:29 -0300

    Initial Setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    contente: [
+        './slices/**/*.{js,ts.vue}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: ['Nunito Sans Variable', 'sans-serif'],
+                heading: ['Nunito Variable', 'sans-serif'],
+            }
+        }
+    }
+}
